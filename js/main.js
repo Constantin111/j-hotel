647c39653e4c7d800ffbca8a8bcd97fc647638d2
Constantin111 <hrom1771@gmail.com>
2020-07-20 09:08:44 +0300

    commit-2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-$(document)
+$(function() {
+    $(document).on("change keyup input click", "input[type='text']", function() {
+        if(this.value.match(/[^0-9]/g)){
+            this.value = this.value.replace(/[^0-9]/g, "");
+        };
+    });
+});
