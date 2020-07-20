5f11d6f9654c2d65ba37f8596b97fafa32362c52
Constantin111 <hrom1771@gmail.com>
2020-07-20 10:08:15 +0300

    commit-5

--- js/main.js
+++ js/main.js
@@ -4,5 +4,38 @@ $(function() {
             this.value = this.value.replace(/[^0-9]/g, "");
         };
     });
+
+    $(document).on("change keyup input click", "#oldDay", function() {
+
+        var thisValue = this.value;
+        if(thisValue > 31){
+            alert('некоректное значение');
+            $(this).val("");
+        };
+    });
+
+    $(document).on("change keyup input click", "#oldMonth", function() {
+
+        var thisValue = this.value;
+        if(thisValue > 12){
+            alert('некоректное значение');
+            $(this).val("");
+        };
+    });
+
+    $(document).on("change keyup input click", "#oldYear", function() {
+
+        var thisValue = this.value;
+        if(thisValue > 2020){
+            alert('некоректное значение');
+            $(this).val("");
+        };
+    });
+
 });
 
+jQuery(function($){
+    $('#oldDay').inputmask('99');
+    $('#oldMonth').inputmask('99');
+    $('#oldYear').inputmask('9999');
+});
