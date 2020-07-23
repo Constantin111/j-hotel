5010b235bf8d8808b3d2285cbb5c8444955212f4
Constantin111 <hrom1771@gmail.com>
2020-07-23 12:13:05 +0300

    commit-7

--- js/main.js
+++ js/main.js
@@ -1,41 +1,61 @@
-$(function() {
-    $(document).on("change keyup input click", "input[type='text']", function() {
-        if(this.value.match(/[^0-9]/g)){
+$(function () {
+    $(document).on("change keyup input click", "input[type='text']", function () {
+        if (this.value.match(/[^0-9]/g)) {
             this.value = this.value.replace(/[^0-9]/g, "");
-        };
+        }
+        ;
     });
 
-    $(document).on("change keyup input click", "#oldDay", function() {
+    $(document).on("change keyup input click", "#oldDay", function () {
 
         var thisValue = this.value;
-        if(thisValue > 31){
+        if (thisValue > 31) {
             alert('некоректное значение');
             $(this).val("");
-        };
+        }
+        ;
     });
 
-    $(document).on("change keyup input click", "#oldMonth", function() {
+    $(document).on("change keyup input click", "#oldMonth", function () {
 
         var thisValue = this.value;
-        if(thisValue > 12){
+        if (thisValue > 12) {
             alert('некоректное значение');
             $(this).val("");
-        };
+        }
+        ;
     });
 
-    $(document).on("change keyup input click", "#oldYear", function() {
+    $(document).on("change keyup input click", "#oldYear", function () {
 
         var thisValue = this.value;
-        if(thisValue > 2020){
+        if (thisValue > 2020) {
             alert('некоректное значение');
             $(this).val("");
-        };
+        }
+        ;
     });
+
+    $(".wellcome-block__date-acces__btn").click(function () {
+        $(".age-gate__block").removeClass('active');
+        $(".post-age-gate__block").addClass("active");
+        new fullpage('#fullpage', {
+            navigation: true,
+            responsiveWidth: 700,
+            anchors: ['home', 'about-us', 'contact'],
+            parallax: true,
+            onLeave: function(origin, destination, direction){
+                console.log("Leaving section" + origin.index);
+            },
+        });
+    })
+
 
 });
 
-jQuery(function($){
+jQuery(function ($) {
     $('#oldDay').inputmask('99');
     $('#oldMonth').inputmask('99');
     $('#oldYear').inputmask('9999');
 });
+
